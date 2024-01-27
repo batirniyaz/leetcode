a552aea73b1f96ac6f7be2026bbf5c3a192fa75f
batir <muratbaevbatirniyaz@gmail.com>
2024-01-27 22:24:25 +0500

    added one more solution

---
+++ 1768-MergeStringsAlternately.py
@@ -0,0 +1,24 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        ans = ""
+        if len(word1) < len(word2):
+            max_word, min_word = word2, word2
+        else:
+            max_word, min_word = word1, word2
+        if len(word1) == len(word2):
+            for i in range(len(max_word)):
+                ans += word1[i] 
+                ans += word2[i]
+        else:
+            for i in range(len(max_word)):
+                if i <= len(min_word)-1:
+                    ans += word1[i]
+                    ans += word2[i]
+                else:
+                    for j in range(i, len(max_word)):
+                        ans += max_word[i:]
+                        return ans
+        return ans
+
+ans = Solution()
+print(ans.mergeAlternately("ab", "pqrs"))
