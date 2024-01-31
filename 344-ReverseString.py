4119946c0e99b4d4bca9d09f052f7ffcf40d698e
batir <muratbaevbatirniyaz@gmail.com>
2024-01-31 17:00:43 +0500

    fixed bug

--- 344-ReverseString.py
+++ 344-ReverseString.py
@@ -1,5 +1,5 @@
 class Solution:
-    def reverseString(self, s: List[str]) -> None:
+    def reverseString(self, s: list[str]) -> None:
         start, end = 0, len(s) - 1
         while start < end:
             s[start], s[end] = s[end], s[start]
