557687e07dd059e87c599091be63e74f77a80aa9
Batir Muratbaev <muratbaevbatirniyaz@gmail.com>
2024-02-04 11:00:09 +0500

    fixed bugs

--- 334-IncreasingTripletSubsequence.py
+++ 334-IncreasingTripletSubsequence.py
@@ -3,22 +3,16 @@ import sys
 
 class Solution:
     def increasingTriplet(self, nums: list[int]) -> bool:
-        min, mid = sys.maxsize, sys.maxsize
-        
-        for j in nums:
-            if j > mid:
+        min = float('inf')
+        mid = float('inf')
+        for num in nums:
+            if num > mid:
                 return True
-            if j < min:
-                min = j
-            elif j < mid:
-                mid = j
-            j += 1
+            if num < min:
+                min = num
+            elif num > min and num < mid:
+                mid = num
         return False
 
 ans = Solution()
 print(ans.increasingTriplet([1,5,0,4,1,3]))
-print(ans.increasingTriplet([4,5,2147483647,1,2]))
-print(ans.increasingTriplet([20,100,10,12,5,13]))
-print(ans.increasingTriplet([0,4,2,1,0,-1,-3]))
-print(ans.increasingTriplet([2,1,5,0,4,6]))
-print(ans.increasingTriplet([2,4,-2,-3]))
