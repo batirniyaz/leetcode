1366311bf3999edb0d6c1b81533cc7238091cbb3
Batir <152086503+batirniyaz@users.noreply.github.com>
2024-02-29 19:26:38 +0500

    Delete easy directory

--- easy/1929-ConcatenationOfArray.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def getConcatenation(self, nums: list[int]) -> list[int]:
-        ans = nums
-        ans.extend(nums)
-        return ans
-    
-ans = Solution()
-print(ans.getConcatenation([1,3,2,1]))
