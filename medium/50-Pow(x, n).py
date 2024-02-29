b568ff19a9833f247d1a6ded8bc9947179d5b15e
Batir <152086503+batirniyaz@users.noreply.github.com>
2024-02-29 19:26:10 +0500

    Delete medium directory

--- medium/50-Pow(x, n).py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def myPow(self, x: float, n: int) -> float:
-        ans = x ** n
-        return ans
-    
-ans = Solution()
-print(ans.myPow(2.00000, 10))
-print(ans.myPow(2.10000, 3))
-print(ans.myPow(2.00000, -2))
